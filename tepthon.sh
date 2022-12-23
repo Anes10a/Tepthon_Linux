a854567ff187379eb9968ddabb236e53147d5503
Source Tepthon <121296616+TheTepthon@users.noreply.github.com>
2022-12-23 16:22:59 +0300

    Update tepthon.sh

--- tepthon.sh
+++ tepthon.sh
@@ -1,6 +1,6 @@
 branch=master
-git clone -b $branch https://github.com/TheTepthon/quran /root/TheTepthon
-cp quran/.env /root/TheTepthon/.env
+git clone -b $branch https://github.com/TheTepthon/Tepthon /root/TheTepthon
+cp Tepthon/.env /root/TheTepthon/.env
 cd /root/TheTepthon
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t quran
-docker run --privileged --env-file .env --rm -i quran
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t Tepthon
+docker run --privileged --env-file .env --rm -i Tepthon
