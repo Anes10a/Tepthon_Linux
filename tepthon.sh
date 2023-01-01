5a69646075bafd0c2f48f5c793dc34b98f40d002
Source Tepthon <121296616+TheTepthon@users.noreply.github.com>
2023-01-01 15:49:10 +0300

    Update tepthon.sh

--- tepthon.sh
+++ tepthon.sh
@@ -1,6 +1,6 @@
-branch=master
-git clone -b $branch https://github.com/TheTepthon/Tepthon /root/TheTepthon
-cp Tepthon/.env /root/TheTepthon/.env
+branch=main
+git clone -b $branch https://github.com/TheTepthon/Svebot /root/TheTepthon
+cp Savebot/.env /root/TheTepthon/.env
 cd /root/TheTepthon
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t Tepthon
-docker run --privileged --env-file .env --rm -i Tepthon
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t Savebot
+docker run --privileged --Savebot-file .env --rm -i Savebot
