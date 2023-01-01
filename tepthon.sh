b9090b0b78b0b865ddc8f8181966f82c96b79346
Anes10a <118900519+Anes10a@users.noreply.github.com>
2023-01-01 17:06:31 +0300

    Update tepthon.sh

--- tepthon.sh
+++ tepthon.sh
@@ -3,4 +3,4 @@ git clone -b $branch https://github.com/TheTepthon/Svebot /root/TheTepthon
 cp Savebot/.env /root/TheTepthon/.env
 cd /root/TheTepthon
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t Savebot
-docker run --privileged --Savebot-file .env --rm -i Savebot
+docker run --privileged --evn-file .env --rm -i Savebot
